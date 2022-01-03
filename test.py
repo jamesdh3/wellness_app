b50a58cf0804599642d75a9450366e2acc7d3a1f
jamesdharv <jamesdharv@gmail.com>
2022-01-03 15:51:32 -0800

    plotting screen working

--- test.py
+++ test.py
@@ -1,69 +1,31 @@
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.uix.floatlayout import FloatLayout
+from kivy.lang import Builder 
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg 
+import matplotlib.pyplot as plt 
 
 
-Builder.load_string('''
-<MenuScreen>:
-    BoxLayout:
-        Button:
-            text: 'Add New Staff'
-            on_press: root.manager.current = 'add_staff'
-        Button:
-            text: 'View Staff Profile'
-        Button:
-            text: 'Salary report'
+# plot stuff 
+# random data points 
+x = [1,2,3,4] 
+y = [5,10,15,20]
 
-<Add_new_staff>:
-    nam: str(name_input)
-    job: job_input
-    GridLayout:
-        cols: 2
-        Label:
-            text: 'Name'
-        TextInput:
-            id: name_input
-            multiline: False
-        Label:
-            text: 'Job'
-        TextInput:
-            id: job_input
-        Label:
-            text: 'Salary'
-        TextInput:
-        Label:
-            text: 'Date of Joining'
-        TextInput:
-        Button:
-            text: 'Back to menu'
-            on_press: root.manager.current = 'menu'
-        Button:
-            text: 'Save'
-            on_press: app.save(name_input.text, job_input.text)
-''')
+plt.plot(x,y)
+
+plt.ylabel('Y axis')
+plt.xlabel('X axis')
 
 
-class MenuScreen(Screen):
-    pass
+class Demo(FloatLayout): 
+    def __init__(self, **kwargs): 
+        super().__init__(**kwargs) 
+        box = self.ids.box 
+        box.add_widget(FigureCanvasKivyAgg(plt.gcf())) 
 
-class Add_new_staff(Screen):
-    pass
 
-sm = ScreenManager()
-sm.add_widget(MenuScreen(name='menu'))
-sm.add_widget(Add_new_staff(name='add_staff'))
+class Main(App): 
+    def build(self): 
+        Builder.load_file('layout_test.kv')
+        return Demo() 
 
-class TestApp(App):
-    def build(self):
-        return sm
-
-    def save(self, name, job):
-        fob = open('c:/test.txt','w')
-        fob.write(name + "\n")
-        fob.write(job)
-        fob.close()    
-
-if __name__ == '__main__':
-    TestApp().run()
+Main().run() 
